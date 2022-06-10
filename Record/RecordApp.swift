606cfcf6917fff4581a3609b65c833fda4e603fa
JIMIN KIM <jim4020key@gmail.com>
2022-06-10 15:37:47 +0900

    Initial Commit

---
+++ Record/RecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordApp.swift
+//  Record
+//
+//  Created by kimjimin on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct RecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
